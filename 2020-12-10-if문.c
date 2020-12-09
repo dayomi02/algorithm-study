577015db57876f7decb47461982493c0576e1e1c
Dayeon Choi <75402494+dayomi02@users.noreply.github.com>
2020-12-10 01:45:24 +0900

    if문 관련 문제풀이

---
+++ 2020-12-10-if문.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+//#define _CRT_SECURE_NOWARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+void main() {
+	/*
+	int a, b;
+	scanf("%d %d", &a, &b);
+	if (a > b)
+		printf(">");
+	else if (a < b)
+		printf("<");
+	else
+		printf("==");
+	
+
+	int sco;
+	scanf("%d", &sco);
+
+	if (0 <= sco && sco <= 100) {
+		if (90 <= sco && sco <= 100) {
+			printf("A");
+		}
+		else if (80 <= sco) {
+			printf("B");
+		}
+		else if (70 <= sco) {
+			printf("C");
+		}
+		else if (60 <= sco) {
+			printf("D");
+		}
+		else {
+			printf("F");
+		}
+	}
+	else {
+		printf("¹üÀ§¸¦ ¹þ¾î³µ½À´Ï´Ù.");
+	}
+	
+
+	int year;
+	scanf("%d", &year);
+	if (1 <= year && year <= 4000) {
+		if (year % 4 == 0) {
+			if (year % 100 != 0) {
+				printf("1");
+				return 0;
+			}
+			if (year % 400 == 0) {
+				printf("1");
+				return 0;
+			}
+		}
+		printf("0");
+	}
+	else {
+		printf("¹üÀ§¸¦ ¹þ¾î³µ½À´Ï´Ù.");
+	}
+
+
+	int x, y;
+	scanf("%d%d", &x, &y);
+	if (x > 0) {
+		if (y > 0)
+			printf("1");
+		else
+			printf("4");
+	}
+	else {
+		if (y > 0)
+			printf("2");
+		else
+			printf("3");
+	}
+
+
+	int H, M;
+	scanf("%d%d", &H, &M);
+	if (0 <= H && H <= 23 && 0 <= M && M <= 59) {
+		if ((M - 45) >= 0) {
+			M = M - 45;
+		}
+		else {
+			if (H == 0) {
+				H = 23;
+				M = 60 - (45 - M);
+			}
+			else {
+				H = H - 1;
+				M = 60 - (45 - M);
+			}
+		}
+		printf("%d %d", H, M);
+	}
+	else {
+		printf("¹üÀ§¸¦ ¹þ¾î³µ½À´Ï´Ù.");
+	}
+	
+		*/
+}
+/*
+
+#include <stdio.h>
+int main() {
+
+
+	return 0;
+}
+
+*/
