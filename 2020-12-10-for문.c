f950cab34234fa564eba51dbe7cc0e283da3c2d1
Dayeon Choi <75402494+dayomi02@users.noreply.github.com>
2020-12-11 01:34:13 +0900

    for문 관련 문제풀이

---
+++ 2020-12-10-for문.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>			//malloc
+
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	/*
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 1; i < 10; i++) {
+		printf("%d * %d = %d\n", n, i, n * i);
+	}
+
+
+	int n, a, b;
+	int *sum;
+	scanf("%d", &n);
+	sum = (int*)malloc(sizeof(int) * n);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d %d", &a, &b);
+		sum[i - 1] = a + b;
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%d\n", sum[i]);
+	}
+
+	free(sum);
+
+
+	int n, x;
+	int* list;
+	scanf("%d %d", &n, &x);
+	list = (int*)malloc(sizeof(int) * n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &list[i]);
+	}
+	for (int i = 0; i < n; i++) {
+		if (list[i] < x)
+			printf("%d ", list[i]);
+	}
+			*/
+
+
+
+
+
+	return 0;
+}
+
+
+/*
+
+#include <stdio.h>
+int main() {
+
+
+	return 0;
+}
+
+*/
